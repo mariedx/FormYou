6839a9338322395a9d78ea2622c79a98f18dec44
MarieDechaux <marie.dechaux@gmail.com>
2021-03-09 16:36:58 +0100

    #2 create devise users with JWT

---
+++ server/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  validates_presence_of :first_name, :last_name
+end
