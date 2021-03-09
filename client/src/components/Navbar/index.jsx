57c343233fc4a8870d67c11448b3be687d459c7e
Coline Brault <coline.brault@gmail.com>
2021-03-09 16:20:25 +0100

    navbar style updated

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -4,8 +4,10 @@ import './style.scss';
 const Navbar = () => (
   <div className="Navbar">
     <Link to="/" className="Navbar__title">FormYou</Link>
-    <Link to="/log-in" className="Navbar__link">Log in</Link>
-    <Link to="/sign-in" className="Navbar__link">Sign up</Link>
+    <div className="Navbar__right">
+      <Link to="/log-in" className="Navbar__link">Connexion</Link>
+      <Link to="/sign-in" className="Navbar__link">Inscription</Link>
+    </div>
   </div>
 );
 
