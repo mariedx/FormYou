af6a7ee5671dd8420e2222f2cd40f839c3adfb18
Mtwod <maximewong@hotmail.com>
2021-03-10 15:49:07 +0100

    #25 Add before_action in controller for author and admin

--- server/app/controllers/api/users_controller.rb
+++ server/app/controllers/api/users_controller.rb
@@ -1,6 +1,9 @@
 class Api::UsersController < Api::BaseController
 
+  before_action :authenticate_user!
   before_action :find_user, only: [ :show, :destroy ]
+  before_action :render_if_not_author_or_admin, only: [ :show, :destroy ]
+  before_action :render_if_not_admin, only: [:index]
 
   def index
     @users = User.all
@@ -29,6 +32,32 @@ class Api::UsersController < Api::BaseController
     {
       status: "200",
       message: "Destroyed successfully!"
+    }
+  end
+
+  def render_if_not_author_or_admin
+    unless current_user == @user || current_user.admin?
+      render json: not_author_data, status: 403
+    end
+  end
+
+  def not_author_data
+    {
+      status: "403",
+      error: "You are not the author of this profile!"
+    }
+  end
+
+  def render_if_not_admin
+    unless current_user.admin?
+      render json: not_admin_data, status: 403
+    end
+  end
+
+  def not_admin_data
+    {
+      status: "403",
+      error: "You are not an admin!"
     }
   end
 end
