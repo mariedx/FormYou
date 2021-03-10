be8d7d73814e2557797cd156def32c10f7dd83e4
MarieDechaux <marie.dechaux@gmail.com>
2021-03-10 16:52:10 +0100

    #5 add seed to create 3 roles

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Role.create(title: "student")
+Role.create(title: "teacher")
+Role.create(title: "admin")
