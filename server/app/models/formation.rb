fe5323b19c5a69c5f56d3308c5cfaaa47f079f52
MarieDechaux <marie.dechaux@gmail.com>
2021-03-10 15:55:28 +0100

    #5 add validations to Formation model

--- server/app/models/formation.rb
+++ server/app/models/formation.rb
@@ -1,3 +1,17 @@
 class Formation < ApplicationRecord
   belongs_to :user
+
+  validate :is_user_teacher
+
+  validates :title,
+  presence: true,
+  length: { minimum: 5 }
+
+  validates :description,
+  presence: true,
+  length: { in: 10..1000 }
+
+  def is_user_teacher
+    user.teacher?
+  end
 end
