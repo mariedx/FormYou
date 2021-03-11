0678f40c044f50c27bebb2b67ac31be9afd99a3b
Clementine <clementine.fouque@gmail.com>
2021-03-11 22:00:56 +0100

    #9 failing at adding the calendar

--- client/src/pages/FormationPage/index.jsx
+++ client/src/pages/FormationPage/index.jsx
@@ -1,12 +1,15 @@
+/* eslint-disable max-len */
+/* eslint-disable no-console */
 import { React, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import API_BASE_URL from '../../api';
 
 const FormationPage = () => {
   const { formationId } = useParams();
-  const [dispalyedFormation, setDisplayedFormation] = useState();
+  const [dispalyedFormation, setDisplayedFormation] = useState({});
+  console.log(dispalyedFormation);
 
-  const fetchFormationData = async () => {
+  const fetchFormationData = () => {
     fetch(`${API_BASE_URL}formations/${formationId}`, {
       method: 'get',
     })
@@ -18,13 +21,27 @@ const FormationPage = () => {
 
   useEffect(() => { fetchFormationData(); }, []);
 
+  const [coursesList, setCoursesList] = useState([]);
+  console.log(coursesList);
+
+  const getCoursesList = () => {
+    fetch(`${API_BASE_URL}courses`, {
+      method: 'get',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setCoursesList(data.filter((course) => course.formation_id !== formationId));
+      });
+  };
+
+  useEffect(() => { getCoursesList(); }, []);
+
   return (
     <div className="FormationPage">
-      <h1>
-        {dispalyedFormation.title}
-        {' '}
-      </h1>
+      <h1>{dispalyedFormation.title}</h1>
       <p>{dispalyedFormation.description}</p>
+      <p>{dispalyedFormation.user_id}</p>
+      <p>{coursesList.map((course) => course.date)}</p>
     </div>
   );
 };
