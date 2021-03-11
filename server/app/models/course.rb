bd3aaa3e5a751412da29a8ff47e4273a9a6572ab
Mtwod <maximewong@hotmail.com>
2021-03-11 11:14:46 +0100

    #8 Add model validations for date

--- server/app/models/course.rb
+++ server/app/models/course.rb
@@ -1,4 +1,21 @@
 class Course < ApplicationRecord
+  validates :date, presence: true
+  validate :send_past_date_error
+
   belongs_to :classroom
   belongs_to :formation
+
+  def in_future?
+    date > Date.today
+  end
+
+  def passed?
+    !in_future?
+  end
+
+  def send_past_date_error
+    unless in_future?
+      errors.add(:date, "must be in future!")
+    end
+  end
 end
