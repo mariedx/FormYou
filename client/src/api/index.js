398653c1e00a806fba2f1150aa6a08800ec7b01d
Mtwod <maximewong@hotmail.com>
2021-03-12 17:33:03 +0100

    Change api base url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api/';
+const API_BASE_URL = 'https://felicita-formyou-server.herokuapp.com/api/';
+// const API_BASE_URL = 'http://localhost:3000/api/';
 
 export default API_BASE_URL;
